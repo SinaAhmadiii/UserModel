9b38f0c2e86788296e2e4c0929bc2e9af4022b9d
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-05-19 14:23:53 +0330

    added init def

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    """A class that representing a user
+
+       args:
+       username: The username of  the user
+       password: The password of the user
+       phone_number: The phone number of the user 
+
+    """
+    def __init__(self,username:str,password:str,phone_number:str = None) -> None:
+    
+        self.username = username
+        self.password = password
+        self.phone_number = phone_number
+        self.id = None
