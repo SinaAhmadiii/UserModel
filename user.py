56ba5dc8c85dd3fb7d16fd59ecb4651da938a52b
SinaAhmadiii <sinaahmadi615@gmail.com>
2023-05-19 16:22:00 +0330

    added docstring

--- user.py
+++ user.py
@@ -8,7 +8,14 @@ class User:
 
     """
     def __init__(self,username:str,password:str,phone_number:str = None) -> None:
-    
+        """
+            Define a new user 
+
+        Args:
+            username (str): The username of user
+            password (str): the password of user
+            phone_number (str, optional): the phone number of user
+        """
         self.username = username
         self.password = password
         self.phone_number = phone_number
@@ -16,20 +23,45 @@ class User:
 
 
     def __str__(self) -> str:
+        """
+        Return a string representation of user    
+        Returns:
+            str: string representation of user
+        """
         return f"Username : {self.username}\nPhone Number : {self.phone_number}"
     
     @staticmethod
     def validate_password(password:str) -> bool:
+        """
+        validate the password
 
+        Args:
+            password (str): the password to validate
+
+        Returns:
+            bool: True if the password is valid, false if password is not valid
+        """
         return len(password) >= 4
     
     @property
     def password(self) -> str:
+        """
+        Get the password of the user
+        Returns:
+            str: The password of the user
+        """
         return self._password
     
     @password.setter
     def password(self,new_password:str) -> None:
+        """
+        Set the password of the user
+        Args:
+            new_password (str): the new password to set
 
+        Raises:
+            ValueError: if the password is not valid
+        """
         if User.validate_password(new_password):
             self._password = new_password
         else:
